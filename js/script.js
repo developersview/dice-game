4a24e790731154cf02018706d54f410e603ed03b
developersview <pcslg1998@gmail.com>
2022-08-15 13:11:29 +0530

    init method code

--- js/script.js
+++ js/script.js
@@ -13,14 +13,24 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 //starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-let currentScore = 0;
-let activePlayer = 0;
-const scores = [0, 0];
-let playing = true;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
+const init = function() {
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    currentScore = 0;
+    activePlayer = 0;
+    scores = [0, 0];
+    playing = true;
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
 
+init();
 const switchPlayer = () => {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
@@ -49,6 +59,7 @@ btnRoll.addEventListener('click', function() {
     }
 });
 
+//holding the score and wining functionality
 btnHold.addEventListener('click', function() {
     if (playing) {
         //1. add current score to active player's score
@@ -56,7 +67,7 @@ btnHold.addEventListener('click', function() {
         document.getElementById(`score--${activePlayer}`).textContent =
             scores[activePlayer];
         //if player score is >=100 , then finish the game
-        if (scores[activePlayer] >= 30) {
+        if (scores[activePlayer] >= 100) {
             playing = false;
             diceEl.classList.add('hidden');
             document
@@ -70,3 +81,5 @@ btnHold.addEventListener('click', function() {
         }
     }
 });
+
+btnNew.addEventListener('click', init);
